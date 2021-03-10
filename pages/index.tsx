2f4981db6d469f08522030596dd7114fb4ae3aa8
Gustavo Costa Neves <guhcostan@gmail.com>
2021-03-10 16:16:57 -0300

    adding google login

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Layout from "../components/Layout"
 import Post, { PostProps } from "../components/Post"
 
 import prisma from '../lib/prisma'
+import {useSession} from "next-auth/client";
 
 
 export const getStaticProps: GetStaticProps = async () => {
@@ -24,8 +25,11 @@ type Props = {
 }
 
 const Blog: React.FC<Props> = (props) => {
+  const [ session, loading ] = useSession()
+  console.log(session)
   return (
     <Layout>
+      <p>Signed in as {session?.user?.email}</p>
       <div className="page">
         <h1>Public Feed</h1>
         <main>
