5ed06fa6562309d825b448befab441c1e0df8c24
Gustavo Costa Neves <guhcostan@gmail.com>
2021-03-10 14:59:02 -0300

    fixing ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,6 @@
 // lib/prisma.ts
 import { PrismaClient } from '@prisma/client'
 
-let prisma: PrismaClient
-
-if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient()
-} else {
-    if (!global.prisma) {
-        global.prisma = new PrismaClient()
-    }
-    prisma = global.prisma
-}
+let prisma = new PrismaClient()
 
 export default prisma
